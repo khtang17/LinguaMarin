5e857795fbca1e4ec36d4449c814fcaa6647efc0
Khanh Tang <KhanhT@khanhs-mbp.attlocal.net>
2018-07-23 21:32:16 -0700

    first commit

---
+++ app/routes.py
@@ -0,0 +1,29 @@
+from app import app
+
+from flask import render_template, request, url_for, redirect, flash
+
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template("home.html")
+
+@app.route('/children')
+def children():
+    return render_template("children.html")
+
+@app.route('/adults')
+def adults():
+    return render_template("adults.html")
+
+@app.route('/resources')
+def resources():
+    return render_template("resources.html")
+
+@app.route('/contact')
+def contact():
+    return render_template("contact.html")
+
+
+
+
